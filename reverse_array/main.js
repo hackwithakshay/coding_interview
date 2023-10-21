a4c6f1eef4ae8694cb7de990ae1c96822dde24f0
hackwithakshay <hackwithakshay@gmail.com>
2023-10-21 12:07:20 +0530

    Save

--- reverse_array/main.js
+++ reverse_array/main.js
@@ -5,3 +5,14 @@
 
 // Output ->[5,4,3,2,1]
 
+const Input = [1,2,3,4,5]
+
+//Method 1
+// const Output = Input.reverse()
+
+// console.log(Output)
+
+//Method 2
+for(let i=Input.length-1;i>=0;i--){
+    console.log(Input[i])
+}
